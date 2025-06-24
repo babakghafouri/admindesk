4c436c4f99cf7140af4bcec210ab4892812e87ca
BabakGhafouri <150241587+babakghafouri@users.noreply.github.com>
2025-06-24 09:57:24 +0330

    Create config.dart

---
+++ flutter/lib/config.dart
@@ -0,0 +1,8 @@
+class Config {
+  // آدرس سرور HBBS و HBBR (Relay و Server اصلی)
+  static const String hbbsServer = "185.255.208.35:21115";
+  static const String hbbrServer = "185.255.208.35:21116";
+
+  // کلید عمومی SSH یا هر کلیدی که نیاز داری (مثال)
+  static const String publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOP6xsREaPjWKFLVrlH9rlDCEp5Hl+I5AxWFYsgR+0lA rustdesk_server";
+}
